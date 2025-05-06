aa8ad6352fefa3335f2bfd7723154299a673e4c8
Megitsune <megitsune14.pro@gmail.com>
2025-05-06 10:03:59 +0200

    Feat/Fix: Add render host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT,
+    allowedHosts: ['graphlab-xbs3.onrender.com'],
+  },
   base: '/',
 })
