6d2b071d7291bd20f524734e9661038663842245
Eric Pascual <eric.g.pascual@gmail.com>
2026-05-21 23:43:54 +0200

    cleanups debug hardcoded settings

--- app/frontend/src/config.js
+++ app/frontend/src/config.js
@@ -1,8 +1,7 @@
 const config = {
-    // apiUrl: import.meta.env.VITE_API_URL || 'http://localhost:3000/api'
-    apiUrl: 'https://portail.terra-numerica.org/graphlab/api'
+    apiUrl: import.meta.env.VITE_API_URL || 'http://localhost:3000/api'
 };
 
-console.log('env: ', import.meta.env);
+console.log('import.meta.env: ', import.meta.env);
 
 export default config;
